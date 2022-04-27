d87c9d031cac2cf29adc7d07a982994af5115bcf
victor.deserto <victor.deserto@agetec.campogrande.ms.gov.br>
2022-04-27 08:21:57 -0400

    init - initial commit;

---
+++ src/components/Navbar.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+defineProps<{
+  isMobile: boolean;
+}>();
+</script>
+
+<style lang="scss" scoped></style>
+
+<template>
+  <div>
+    <div v-if="isMobile">
+      <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+          <h5 class="text-white h4">Collapsed content</h5>
+          <span class="text-muted">Toggleable via the navbar brand.</span>
+        </div>
+      </div>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarToggleExternalContent"
+            aria-controls="navbarToggleExternalContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+    </div>
+    <div v-else>
+      <header>
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Fixed navbar</a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarCollapse"
+              aria-controls="navbarCollapse"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+              <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"
+                    >Disabled</a
+                  >
+                </li>
+              </ul>
+              <form class="d-flex">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main class="container">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active"></div>
+        <div class="carousel-item">
+          <img src="@/assets/logo.svg" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src="@/assets/logo.svg" class="d-block w-100" alt="..." />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
